d0d1ae329fc014fc2085cd4231027e596f109e54
yiendos <yiendos@sbmatrix.com>
2020-05-16 09:17:23 +0100

    make paths dependant on repo env details

--- .gitpod/bootstrap.sh
+++ .gitpod/bootstrap.sh
@@ -8,14 +8,14 @@ joomla plugin:install joomlatools/console-joomlatools:dev-master
 
 echo "* Create a new Joomla site"
 
-joomla site:create joomla.${JOOMLA_VERSION} --disable-ssl --mysql-login=root: --www=/workspace/joomlatools-gitpod/ --release=${JOOMLA_VERSION}
+joomla site:create joomla.${JOOMLA_VERSION} --disable-ssl --mysql-login=root: --www=$GITPOD_REPO_ROOT/ --release=${JOOMLA_VERSION}
 
-if [[ -e /workspace/joomlatools-gitpod/.gitpod/composer.json ]]; then
+if [[ -e $GITPOD_REPO_ROOT/.gitpod/composer.json ]]; then
   echo "* Installing user defined composer requirements"
 
-  cp /workspace/joomlatools-gitpod/.gitpod/composer.json /workspace/joomlatools-gitpod/joomla.${JOOMLA_VERSION}/composer.json
+  cp $GITPOD_REPO_ROOT/.gitpod/composer.json $GITPOD_REPO_ROOT/joomla.${JOOMLA_VERSION}/composer.json
 
-  composer update --working-dir=/workspace/joomlatools-gitpod/joomla.${JOOMLA_VERSION} --ignore-platform-reqs
+  composer update --working-dir=$GITPOD_REPO_ROOT/joomla.${JOOMLA_VERSION} --ignore-platform-reqs
 fi
 
 apachectl start
