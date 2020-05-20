b3cce7cdf89c9833eb4e3b070026f1562da0823a
yiendos <yiendos@sbmatrix.com>
2020-05-20 10:02:13 +0100

    #1: step 1 allow user to define Joomla versions, if not install the latest

--- .gitpod/bootstrap.sh
+++ .gitpod/bootstrap.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+#@todo maybe want to secure this more https://wiki.bash-hackers.org/howto/conffile#secure_it
+#load up user predefine variables
+source $GITPOD_REPO_ROOT/config.sh
+
 echo "* About to set up the gitpod area"
 
 export PATH=/home/gitpod/.composer/vendor/bin/:$PATH
@@ -10,7 +14,13 @@ joomla plugin:install joomlatools/console-joomlatools:dev-master
 
 echo "* Create a new Joomla site"
 
-joomla site:create ${APACHE_DOCROOT_IN_REPO} --disable-ssl --mysql-login=root: --www=$GITPOD_REPO_ROOT/
+release="--release=latest"
+
+if [ -n "$joomla" ]; then
+  release="--release=$joomla"
+fi;
+
+joomla site:create ${APACHE_DOCROOT_IN_REPO} --disable-ssl --mysql-login=root: --www=$GITPOD_REPO_ROOT/ $release
 
 if [[ -e $GITPOD_REPO_ROOT/.gitpod/composer.json ]]; then
   echo "* Installing user defined composer requirements"
---
+++ .gitpod/config.sh
@@ -0,0 +1,2 @@
+#joomla=3.8.9
+#repo=http://github.com/yiendos/swiss.snowboard.social
