76188fa04e573f5d594a9d1b929c69d7150b723c
yiendos <yiendos@sbmatrix.com>
2020-05-20 09:25:33 +0100

    #1: step 1 remove complicated site+version number

--- .gitpod/bootstrap.sh
+++ .gitpod/bootstrap.sh
@@ -10,14 +10,14 @@ joomla plugin:install joomlatools/console-joomlatools:dev-master
 
 echo "* Create a new Joomla site"
 
-joomla site:create joomla.${JOOMLA_VERSION} --disable-ssl --mysql-login=root: --www=$GITPOD_REPO_ROOT/ --release=${JOOMLA_VERSION}
+joomla site:create ${APACHE_DOCROOT_IN_REPO} --disable-ssl --mysql-login=root: --www=$GITPOD_REPO_ROOT/
 
 if [[ -e $GITPOD_REPO_ROOT/.gitpod/composer.json ]]; then
   echo "* Installing user defined composer requirements"
 
-  cp $GITPOD_REPO_ROOT/.gitpod/composer.json $GITPOD_REPO_ROOT/joomla.${JOOMLA_VERSION}/composer.json
+  cp $GITPOD_REPO_ROOT/.gitpod/composer.json $GITPOD_REPO_ROOT/${APACHE_DOCROOT_IN_REPO}/composer.json
 
-  composer update --working-dir=$GITPOD_REPO_ROOT/joomla.${JOOMLA_VERSION} --ignore-platform-reqs
+  composer update --working-dir=$GITPOD_REPO_ROOT/${APACHE_DOCROOT_IN_REPO} --ignore-platform-reqs
 fi
 
 apachectl start
