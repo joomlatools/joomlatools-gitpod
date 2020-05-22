7a6bb17f0a5abb43b88f69740b6b0aab2572a428
yiendos <yiendos@sbmatrix.com>
2020-05-22 13:45:31 +0100

    #1: next up lets preconfig a selection of components

--- .gitpod/config.sh
+++ .gitpod/config.sh
@@ -1,3 +1,3 @@
 #joomla=3.8.9
 repo=https://github.com/joomlatools/joomlatools-platform.git
-#composer="aws/aws-sdk-php phpoffice/phpexcel"
+composer="joomlatools/platform-media joomlatools/platform-finder joomlatools/platform-content joomlatools/platform-categories"
