b74dffb55d50a3af3885919c5cd92707efbe2a88
yiendos <yiendos@sbmatrix.com>
2021-05-20 16:38:19 +0100

    #11: re-comment configuration file

--- .gitpod/config.sh
+++ .gitpod/config.sh
@@ -1,4 +1,4 @@
-joomla_beta=4.0-dev
+#joomla_beta=4.0-dev
 #joomla=3.8.9
 #repo=https://github.com/joomlatools/joomlatools-platform.git
 #composer="joomlatools/platform-media joomlatools/platform-finder joomlatools/platform-content joomlatools/platform-categories"
